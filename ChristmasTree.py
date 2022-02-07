35d56e211972c5cdc45307147dcc26c3a6787273
Rashmi Singh <rashmi@Rashmis-MacBook-Air.local>
2022-02-06 23:33:26 -0700

    adding starting project

---
+++ ChristmasTree.py
@@ -0,0 +1,21 @@
+# Everything in Python is an object that inherits from base parent.
+
+
+
+matrix = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+]
+
+for index in matrix:
+    for col in index:
+        if col == 0:
+            print(' ', end=' ')
+        elif col == 1:
+            print('*', end=' ')
+    print(' ')
